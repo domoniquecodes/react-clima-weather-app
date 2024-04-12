5976c6fd392edefbda8de56ba22c16b3878e14c2
domoniquecodes <domonique.wilson89@icloud.com>
2024-04-12 11:32:39 -0400

    adjujsted package json file & adding bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://domonique-wilson-portfolio.netlify.app/about">
-          Domonique Wilson
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/domoniquecodes/react-clima-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://domonique-wilson-portfolio.netlify.app/about"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Domonique Wilson
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/domoniquecodes/react-clima-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
